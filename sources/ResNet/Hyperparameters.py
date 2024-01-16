564927cb183658edff146584593b0522acba7151
Your Name <you@example.com>
2024-01-15 20:37:49 -0500

    add easy functions to create hp args

--- sources/ResNet/Hyperparameters.py
+++ sources/ResNet/Hyperparameters.py
@@ -15,3 +15,36 @@ class Hyperparameters:
         print("batch_size:", self.batch_size)
         print("learning_rate:", self.learning_rate)
         print("num_classes:", self.num_classes)
+
+
+def get_hp_formal_cifar10():
+    hp = Hyperparameters()
+    hp.data_size_train = 50000
+    hp.data_size_test = 10000
+    hp.num_epochs = 20
+    hp.batch_size = 128
+    hp.learning_rate = 0.01
+    hp.num_classes = 10
+    return hp
+
+
+def get_hp_test_cifar10_fast():
+    hp = Hyperparameters()
+    hp.data_size_train = 128
+    hp.data_size_test = 128
+    hp.num_epochs = 1
+    hp.batch_size = 128
+    hp.learning_rate = 0.01
+    hp.num_classes = 10
+    return hp
+
+
+def get_hp_test_cifar10():
+    hp = Hyperparameters()
+    hp.data_size_train = int(1e4)
+    hp.data_size_test = int(2e3)
+    hp.num_epochs = 1
+    hp.batch_size = 128
+    hp.learning_rate = 0.01
+    hp.num_classes = 10
+    return hp
