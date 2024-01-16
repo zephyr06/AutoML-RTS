bd88eec9fb684c9c422562b7b933f6e27720e875
Your Name <you@example.com>
2024-01-15 19:02:44 -0500

    rm repeated folder

--- TorchPruner/wiki40b.py
+++
@@ -1,4 +0,0 @@
-from datasets import load_dataset
-
-imdb_dataset = load_dataset("wiki40b", "en")
-imdb_dataset
--- learn_hf/learn_something.py
+++
@@ -1,5 +0,0 @@
-from datasets import load_dataset
-from transformers import AutoTokenizer, DataCollatorWithPadding
-
-raw_datasets = load_dataset("glue", "mrpc")
-print(raw_datasets["train"][15])
