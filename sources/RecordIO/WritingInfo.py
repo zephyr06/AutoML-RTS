79546ac8d07ebb32f3d4f4442be45f753a0be3cb
zephyr06 <sen.zephyr.wang@gmail.com>
2024-01-17 14:40:29 -0500

    fix constructor overload bug

--- sources/RecordIO/WritingInfo.py
+++ sources/RecordIO/WritingInfo.py
@@ -1,19 +1,19 @@
 
 class WritingInfo:
-    def __init__(self, pruning_ratio=0, accuracy=0, latency=0, training_data_noise=0, model_name=""):
+    def __init__(self, pruning_ratio=-1, accuracy=-1, latency=-1, training_data_noise=0, model_name="", profile_csv_file_name: str = None):
         self.pruning_ratio = pruning_ratio
         self.accuracy = accuracy
         self.latency = latency
-        self.training_data_noise = training_data_noise
-        self.model_name = model_name
-    def __init__(self, profile_csv_file_name: str):
-        self.pruning_ratio = -1
-        self.accuracy = -1
-        self.latency = -1
 
-        parts = profile_csv_file_name.split('_')
-        self.model_name = parts[2]
-        self.training_data_noise = parts[4]
+        if profile_csv_file_name:
+            parts = profile_csv_file_name.split('_')
+            self.model_name = parts[2]
+            self.training_data_noise = parts[4]
+        else:
+            self.training_data_noise = training_data_noise
+            self.model_name = model_name
+
+
 
     
 def get_output_file_name(model_name, training_data_noise):
