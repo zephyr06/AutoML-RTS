82a663b5f4d415ff292a7eb07f458a6ffb6682c4
Your Name <you@example.com>
2024-01-29 16:25:52 -0500

    add draw latency option to draw_multiple_profile_data

--- scripts/Visualization/draw_multiple_profile_data.py
+++ scripts/Visualization/draw_multiple_profile_data.py
@@ -5,7 +5,15 @@ import os
 from ResNet.variables import ROOT_PATH
 import seaborn as sns
 from utils import read_df_from_csv, marker_styles, get_all_profile_files
+import argparse
 
+parser = argparse.ArgumentParser(description='Draw multiple profile data.')
+parser.add_argument('--draw_option', default='accuracy',
+                    help='accuracy or latency')
+
+args = parser.parse_args()
+
+option = args.draw_option
 
 csv_files = get_all_profile_files("profile_data_resnet18_noise_0.*.csv")
 file_num = len(csv_files)
@@ -16,14 +24,25 @@ fig, ax1 = plt.subplots()
 
 for i in range(file_num):
     df = read_df_from_csv(csv_files[i])
-    sns.lineplot(data=df, x="Pruning_ratio", y="Accuracy",
-                 marker='o', ax=ax1,  color=colors[i], label='Noise: 0.'+str(i), markers=marker_styles[i])
-
+    if option == 'accuracy':
+        sns.lineplot(data=df, x="Pruning_ratio", y="Accuracy",
+                     marker='o', ax=ax1,  color=colors[i], label='Noise: 0.'+str(i), markers=marker_styles[i])
+    elif option == 'latency':
+        sns.lineplot(data=df, x="Pruning_ratio", y="Latency",
+                     marker='o', ax=ax1,  color=colors[i], label='Noise: 0.'+str(i), markers=marker_styles[i])
+    else:
+        raise ValueError('Invalid draw option.')
 
 ax1.set_xlabel('Pruning Ratio')
-ax1.set_ylim([20, 100])
 ax1.set_xlim([-0.05, 1.0])
-ax1.set_ylabel('Accuracy (%)')
+if option == 'accuracy':
+    ax1.set_ylabel('Accuracy (%)')
+    ax1.set_ylim([20, 100])
+elif option == 'latency':
+    ax1.set_ylabel('Latency (ms)')
+
+else:
+    raise ValueError('Invalid draw option.')
 
 ax1.grid(True, linestyle='--')  # Add dashed grid lines
 plt.legend()
