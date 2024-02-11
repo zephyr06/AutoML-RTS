d9ee62b3e4b1054d73dda03b5894d93a4abcda35
Your Name <you@example.com>
2024-02-11 15:36:16 -0500

    add notes

--- scripts/profile_noise_level.sh
+++ scripts/profile_noise_level.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# NOTE: run this file at the root folder of the project
 for noise_level in $(seq 0.0 0.1 0.9); do
     python scripts/profile_torch_pruner.py --training_poison_chance 0 --testing_data_noise $noise_level
 done
